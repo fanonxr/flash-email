7b312c63747feea6f9ee113540daf66957a85a9c
Fanon X Rogers <fanonxrogers@Fanons-MacBook-Pro.local>
2023-03-18 21:09:29 -0400

    first integ test

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use flashmail::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    run(listener)?.await
 }
